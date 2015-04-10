fc1a667c8af84d65feb5b76bff56eb572b5dd294
gdh1995 <gdh1995@qq.com>
2015-04-10 16:23:04 +0800

    update mysql's password

--- wechat_wall/wechat_wall/settings.py
+++ wechat_wall/wechat_wall/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'wx2_wall',
         'USER': 'root',
-        'PASSWORD': 'admin',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
